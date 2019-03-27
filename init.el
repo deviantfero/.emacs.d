0b3c08f18a752c39eba43e78919db6e9b05cc6df
deviantfero <fmorataya.04@gmail.com>
2019-03-27 15:30:07 -0600

    group plugins in a more logical maner

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 ;;; package -- deviantfero's .init.el -*- lexical-binding: t; -*-
+
 ;;; Commentary:
 ;;; Code:
 
@@ -25,7 +26,7 @@
     (when (and eslint (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
 
-(defun sudo-save ()
+(defun sudo-write ()
   "Use TRAMP to open a file with write access using sudo."
   (interactive)
   (if (not buffer-file-name)
@@ -58,7 +59,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (define-globalized-minor-mode global-outline-minor-mode
   outline-minor-mode outline-minor-mode)
 
-
 ;;; ORG MODE
 (use-package org
   :ensure t
@@ -85,19 +85,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :ensure t
   :config (minions-mode 1))
 
-(use-package smart-mode-line
-  :ensure t
-  :init
-  (setq sml/no-confirm-load-theme t)
-  :config
-  (setq sml/line-number-format "%4l")
-  (setq sml/name-width 40)
-  (setq sml/mode-width 'full)
-  (setq sml/no-confirm-load-theme t)
-  (column-number-mode)
-  (smart-mode-line-enable 1))
-
-;;; BASIC
+;;; HELM
 (use-package helm
   :after evil
   :ensure t
@@ -117,6 +105,15 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (use-package helm-ag
   :ensure t)
 
+(use-package helm-projectile
+  :ensure t
+  :after projectile)
+
+;;; BASIC
+(use-package highlight-numbers
+  :ensure t
+  :config (highlight-numbers-mode 1))
+
 (use-package projectile
   :after (evil helm)
   :ensure t
@@ -126,10 +123,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
               ("C-g" . helm-projectile))
   :config
   (projectile-mode +1))
-
-(use-package helm-projectile
-  :ensure t
-  :after projectile)
 
 (use-package magit
   :after evil
@@ -160,8 +153,10 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (use-package yasnippet
   :ensure t
-  :config
-  (yas-global-mode 1))
+  :config (yas-global-mode 1)
+  :bind (:map yas-keymap
+              ("TAB" . nil)
+              ("C-o" . yas-next-field-or-maybe-expand)))
 
 (use-package which-key
   :ensure t
@@ -178,6 +173,160 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (setq tramp-use-ssh-controlmaster-options nil)
   (setf tramp-persistency-file-name
         (concat temporary-file-directory "tramp-" (user-login-name))))
+
+;;; EVIL
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  :ensure t
+  :config
+  (evil-mode 1)
+  (define-key evil-normal-state-map (kbd "<f5>") 'compile)
+  (define-key evil-normal-state-map (kbd "<f7>") 'my/multi-term)
+  (define-key evil-normal-state-map (kbd "-") 'dired-jump)
+  (define-key evil-normal-state-map (kbd "ga") 'align-regexp)
+  (define-key evil-normal-state-map (kbd "gt") 'other-frame)
+  (define-key evil-normal-state-map (kbd "C-w n") 'make-frame-command)
+  (define-key evil-normal-state-map (kbd "C-w u") 'winner-undo)
+  (define-key evil-normal-state-map (kbd "C-w C-r") 'winner-redo)
+  (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)
+  (define-key evil-normal-state-map (kbd "<backtab>") 'org-global-cycle)
+  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down))
+
+(use-package evil-collection
+  :ensure t
+  :custom (evil-collection-setup-minibuffer t)
+  :init (evil-collection-init))
+
+(use-package evil-org
+  :ensure t
+  :hook
+  (org-mode . evil-org-mode)
+  (evil-org-mode . (lambda () (evil-org-set-key-theme)))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(use-package evil-magit
+  :after (evil magit)
+  :ensure t)
+
+(use-package evil-surround
+  :after evil
+  :ensure t
+  :config (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :after evil
+  :ensure t
+  :config (evil-commentary-mode 1))
+
+(use-package evil-matchit
+  :ensure t
+  :config (global-evil-matchit-mode 1))
+
+(use-package evil-visualstar
+  :ensure t
+  :config (global-evil-visualstar-mode))
+
+;;; LANGUAGES.
+(use-package web-mode
+  :ensure t
+  :mode (("\\.ejs\\'" . web-mode)
+         ("\\.json\\'" . web-mode))
+  :config
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-block-padding 2)
+  (setq web-mode-style-padding 2))
+
+(use-package js2-mode
+  :ensure t
+  :init
+  (setq js2-mode-show-strict-warnings nil)
+  (setq js2-mode-show-parse-errors 1))
+
+(use-package flow-minor-mode
+  :ensure t
+  :hook (js-mode . flow-minor-enable-automatically))
+
+(use-package flow-js2-mode
+  :load-path "./modes/"
+  :hook (flow-minor-mode . flow-js2-mode))
+
+(use-package elpy
+  :ensure t
+  :hook ((python-mode . elpy-mode)
+         (python-mode . flycheck-mode)))
+
+(use-package restclient
+  :ensure t
+  :mode (("\\.http\\'" . restclient-mode)
+         ("\\.rest\\'" . restclient-mode))
+  :config
+  (setq outline-regexp "#[*\f]+"))
+
+(use-package go-mode
+  :ensure t
+  :mode ("\\.go\\'"))
+
+(use-package haskell-mode
+  :ensure t)
+
+(use-package elixir-mode
+  :ensure t)
+
+(use-package cmake-mode
+  :ensure t)
+
+(use-package rjsx-mode
+  :after js2-mode
+  :mode "\\.js\\'"
+  :interpreter "node"
+  :ensure t)
+
+(use-package indium
+  :ensure t
+  :config
+  (setq indium-chrome-executable "google-chrome-stable"))
+
+(use-package haml-mode
+  :ensure t
+  :mode "\\.haml\\'")
+
+(use-package irony
+  :ensure t
+  :hook
+  (c++-mode . irony-mode)
+  (c-mode . irony-mode)
+  (objc-mode . irony-mode))
+
+(use-package conf-mode
+  :ensure t
+  :mode (("\\.env\\'" . conf-mode))
+  :config
+  (setq outline-regexp "[#\f]+")
+  (define-key evil-normal-state-map (kbd "TAB") 'org-cycle))
+
+(use-package sh-script
+  :config
+  (setq outline-regexp "[#\f]+"))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package grammar-mode
+  :load-path "./modes/"
+  :mode "\\.grm\\'" )
 
 ;;; COMPANY
 (use-package company
@@ -228,11 +377,15 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :hook
   (restclient-mode . (lambda () (add-to-list 'company-backends 'company-restclient))))
 
+(use-package company-go
+  :ensure t
+  :hook
+  (go-mode . (lambda () (add-to-list 'company-backends 'company-go))))
+
 (use-package intero
   :ensure t
   :hook
   (haskell-mode . intero-mode))
-
 
 ;;; FLYCHECK
 (use-package flycheck
@@ -254,140 +407,9 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :config
   (flycheck-irony-setup))
 
-;;; EVIL
-(use-package evil
-  :init
-  (setq evil-want-keybinding nil)
-  :ensure t
-  :config
-  (evil-mode 1)
-  (setq evil-mode-line-format '(before . mode-line-front-space))
-  (define-key evil-normal-state-map (kbd "<f5>") 'compile)
-  (define-key evil-normal-state-map (kbd "<f7>") 'my/multi-term)
-  (define-key evil-normal-state-map (kbd "-") 'dired-jump)
-  (define-key evil-normal-state-map (kbd "ga") 'align-regexp)
-  (define-key evil-normal-state-map (kbd "gt") 'other-frame)
-  (define-key evil-normal-state-map (kbd "C-w n") 'make-frame-command)
-  (define-key evil-normal-state-map (kbd "C-w u") 'winner-undo)
-  (define-key evil-normal-state-map (kbd "C-w C-r") 'winner-redo)
-  (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)
-  (define-key evil-normal-state-map (kbd "<backtab>") 'org-global-cycle)
-  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down))
-
-(use-package evil-collection
-  :ensure t
-  :custom (evil-collection-setup-minibuffer t)
-  :init (evil-collection-init))
-
-(use-package evil-org
-  :ensure t
-  :hook
-  (org-mode . evil-org-mode)
-  (evil-org-mode . (lambda () (evil-org-set-key-theme)))
-  :config
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
-
-(use-package evil-magit
-  :after (evil magit)
+(use-package flycheck-flow
+  :after (flow-minor-mode)
   :ensure t)
-
-(use-package evil-surround
-  :after evil
-  :ensure t
-  :config (global-evil-surround-mode 1))
-
-(use-package evil-commentary
-  :after evil
-  :ensure t
-  :config (evil-commentary-mode 1))
-
-;;; LANGUAGES.
-(use-package racket-mode
-  :ensure t
-  :mode ("\\.rkt\\'" . racket-mode))
-
-(use-package restclient
-  :ensure t
-  :mode (("\\.http\\'" . restclient-mode)
-         ("\\.rest\\'" . restclient-mode))
-  :config
-  (setq outline-regexp "#[*\f]+"))
-
-(use-package haskell-mode
-  :ensure t)
-
-(use-package elixir-mode
-  :ensure t)
-
-(use-package elpy
-  :ensure t
-  :hook ((python-mode . elpy-mode)
-         (elpy-mode . flycheck-mode)
-         (elpy-mode . (lambda ()
-                        (add-to-list 'company-backends 'elpy-company-backend)))))
-
-(use-package cmake-mode
-  :ensure t)
-
-(use-package js2-mode
-  :ensure t
-  :init
-  (setq js2-mode-show-strict-warnings nil)
-  (setq js2-mode-show-parse-errors 1))
-
-(use-package rjsx-mode
-  :after js2-mode
-  :mode "\\.js\\'"
-  :interpreter "node"
-  :ensure t)
-
-(use-package indium
-  :ensure t
-  :config
-  (setq indium-chrome-executable "google-chrome-stable"))
-
-(use-package json-mode
-  :ensure t)
-
-(use-package vue-mode
-  :ensure t)
-
-(use-package irony
-  :ensure t
-  :hook
-  (c++-mode . irony-mode)
-  (c-mode . irony-mode)
-  (objc-mode . irony-mode))
-
-(use-package conf-mode
-  :ensure t
-  :mode (("\\.env\\'" . conf-mode))
-  :config
-  (setq outline-regexp "[#\f]+")
-  (define-key evil-normal-state-map (kbd "TAB") 'org-cycle))
-
-(use-package sh-script
-  :config
-  (setq outline-regexp "[#\f]+"))
-
-(use-package yaml-mode
-  :ensure t)
-
-(use-package grammar-mode
-  :load-path "./modes/"
-  :mode "\\.grm\\'" )
-
-(use-package flow-minor-mode
-  :ensure t
-  :hook (js-mode . flow-minor-enable-automatically))
-
-(use-package flow-js2-mode
-  :load-path "./modes/"
-  :hook (flow-minor-mode . flow-js2-mode))
 
 ;;; NOTE-TAKING
 (use-package pdf-tools
@@ -421,7 +443,9 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (use-package ox-pandoc
   :after org
   :ensure t)
+
 ;;; OTHER MODES
+(column-number-mode 1)
 (show-paren-mode 1)
 (menu-bar-mode -1)
 (toggle-tool-bar-mode-from-frame -1)
@@ -442,11 +466,9 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
       '(("es_ES" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8)
         ("en_EN" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8))
       ispell-dictionary "es_ES")
-
+(setq blink-matching-paren nil)
 (toggle-scroll-bar nil)
 (load-theme 'wpgtk t)
-
-
 
 ;;; AUTOMATICALLY GENERATED
 (custom-set-variables
@@ -460,9 +482,10 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  '(custom-enabled-themes (quote (wpgtk)))
  '(custom-safe-themes
    (quote
-    ("db8ca043766350e6d67ee88fe584e9bb60b533226af797180e206d8e6b4cc74a" "a31f35e3972930298c8d3264119706b0003ca07ee3485d9630fe298b56c21fae" "aa8d8edab433fe0b807eee7cb12405386d8db67722247c1941a1402135b00cce" "1bc83a6814691bcc491c40183b4d1cb77a02444271efc6bf78ce6abe0b9c517d" "801eaa366cf1dfa90d3adc981cc076eed222898dd5039716a450a3a4caaab878" "960ba390dfe7379c862bbd410170b539d0544d721a6d2f20cb9427b566422cdd" "9e90ce9d5db920162806efa86205683b59f5e2bf28da116792f4dd50ffdad257" "903df0a40fcf9d16ed22897c606d2ec3a451fd28ad2fe33799fabb004193c53e" "91dcc29f93f9ab13b0e264a67f7541eb731bf75906f2f1c88880fe286226bcbb" "2733b5f7898ae398e7f529d2705aa7e6fbe065e64c4ab8b2097c7fdb2531523c" "b9972b4c939cc6c5e6b5d566c1723d29f88a92d81f9526f4fd30d091ab48c78f" "29f3a4dc52c562c4f674b60d2d34b7ca56dab239350401a082c66c71e1decd94" "07af38388ff48e5454d8d5b79a8bd1d58620787a5f85b730528c777b97ae1b1b" "eaff3bcdd83286816c3ec35eed15ffa73f7a178cd36e7abe0bbddea1ea689d4e" "d99b11db38e5529d3c1e04636692fa82270d08927223b6c5231e33ba61d5a76b" "79fde24ae16dd2a8fd68b6240c54f1abc5dab59ad12099e97abbe073d3c53de5" "a6986fd50dd556c8e59184cebe787b6e8d6da847624ba0cc07f7a9a6240ab287" "24236ff6bf291ccc586229cd7de9b1c880ec57c297f1271c2f49b176505f8bda" "068802bf7ba36fb6ac9890ef569306dbc2787df8977ea1425ac0e5f5947857de" "a78c0443d8e7d24c41c71b18e4f159ccd2049455cd3d39f917449b6218678471" "963d25e363ad0d65803fd79da6d27d84c68cd0fa509bf16461f72b70cd437b9c" "7eaa3ac3cf1360b7bde6927cf1ff0f4cb02da5f59bf6d091b1b52d85496a5cdc" "d51bb671105baa4972b9c9ee726b8e259a1df9c9e5849f041e231b41af53cd72" "ec1a7db8a692f390d4586950159d099f544f0671c6b84acd245e0c99c6b67756" "d82be0f6d159afc8d3454341b61a85726f540acc1120173efcf13cb685a780e4" "b2d3cc213eb601663964102aa21f56d46ece7f15ef60a87fbd1c6212139cf5ac" "f91f0c73603d8007e81812342698f0e97eca5ef5ee17b9d240c35ea86cdd04a9" "b7d13664d4c1ab4f5043560012f47c48b3975a3b76cb7aa0513f4e9b44084ff4" "3c362bc8b8641f8674a25c0764918c43404343e8f0c507d842a0d39213ca004f" "31727b11df14e9c72e26e5fe54be088bc64a46bad4cc1c3c1668b21e401d8dbd" "01ac01d5685319b831ba8da231a5445e6731359c5d086eca8dbbf616475edbc4" "9851704f68d5dc28a6906c9b82bb624c9b02c0f56111d662cc5cc8549d61bdc9" "d77b456aeabc9d5686f3a0e55200f96a7dc1e44258f546e5e99d6ae9b3a8e69f" "1d45727504db1f6528410b9f8fbcea82b1e3ff0741000278311a8394c7876cf1" "cbf94cf658e1f858a39b63670c7c326cc838fa1bd1170d097f0fd335adc81df9" "201c7538c3196f8f5f1c949e72f033eabad77622e9bbee600d5e457d6dfdd570" "5f295bb1303e8ea912e7fab806008eb9dc584d772eff4f42959bc15ba766f5ce" "f3a15f89d7e0062256c5eff2cf72a854e5bec72cc6b203a6e785bf3a27f81cfe" "c499720368e09a56ec362ee23c7a44202419108844ef7c09c623bc10bbacde77" "28351cdec72c9628101e0d82a0616628ff7ffbe29160366d7d8189b24330c430" "9724a104d6a69ed0c2cc90842b6881e9f2985b3e205dd96b9be403c11c79a22a" "6e2c04db23101a0cce6645fc622ce90b933e41216f862b4267c2f175e755207b" "fa22f350fb34c44363a26e7c8742c528f72f235b08a1c59731617c1f59595427" "e53709dc67a9345e89967782b1f43670b5330e9778a24f0ec173da789a42227c" "70a9a887bd6f7153fbabe60b01bf7ca6688e1e34b41a731a18b87d6ec1d97fe5" "c3d103e1d95596aa17df3fe5adbe6b780b1f6dfd5e13f3d03cf359edcec4f36e" "08c3aa56c6d9211e6c942a8a4e21c88ae46cdc903c2539bb543ed3012fd06b44" "c15e8e951ff27e9ac6311a71d94e52a66f6465e37b3acda410774ad9f4731a5d" "0c85de10851cf504c53a79d7af40c901b321ceda695c7b2f09ac1d9c08a91ea7" "ea27b445edb93b38cf8c6f087e188eacb6b3a389ff78473987e69c82f29c9200" "8aa7251074b0f3304948e6bad6540c9cec964112677884cd39c32ac2e480c2ae" "f9376799d2ac86f104b58e9d4742677c66808cdd2b12cb0f3e2364d0e771279b" "a1d1c523e2b234464b19090cd27c70d22b85840dd40614cd287457210ed3620c" "6e562c29348052159de4e6b8e86df36e1eea300b98454e92ae03b0fd8e141271" "d140e64e4e1c29a564ead7104dd08d063e7432334144bc16280b3cba49fcfeb2" "8c62da104514ece86adc596d38d403b8c07d0107685b197adbcdf9ef226c18a6" "4c9363a2de6807560eb127a4e79e7cf2afff4505ca112e22ec187236ad4b8b51" "a9f5ae1da25e28d22850dd9542b078b2563de99530b898b23c2394e724f9d65c" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6b66f49729b5dc928b8c793581064254a1788fddb1eaa0b7bfd807494f36c8a9" default)))
+    ("6ca2c4437d37cea1cb151749c3585d1ea29fd483b831f7013beeac90081101c2" "07839198ca42eaaa51a28432cb8658ec5fbadbd8a6e6da231f116f7841fee0cb" "51517dac0b5f6b9839b8c2e01e07ada1c9c2440de21eb1fa534718f9aaf13f19" "4fdd3bae4ef37b5b67dccc91557bf069a8ba804723f49b462d69e5a64e298dee" "1caa46cdd3573072d3ac92a2c21474c2e1dad65de9b6d0c7ede64c79e9879f1a" "0d4da8e06207a8ac31c6e8335bcf1481f44f9eb30c065649ae8a9d46803f10ea" "b13ffeb4813ebc7b964dcc39b7c0d77704005acbaf1fcb4ca42e9bb942c82b7a" "ac2ce00fab91c5799e8289dc6be38696efcbf4489eab689e7debfc3e8e3ecc93" "bb6ce5371e43d4fefc4613af95fb2bdca1846c8475df1ee6e5fc520f930a2150")))
  '(electric-pair-mode t)
  '(evil-collection-setup-minibuffer t)
+ '(flycheck-javascript-flow-args nil)
  '(global-evil-mc-mode t)
  '(global-origami-mode t)
  '(magit-diff-section-arguments (quote ("--no-ext-diff")))
@@ -471,7 +494,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     ("/home/fernando/org/reggie.org" "/home/fernando/org/todo.org")))
  '(package-selected-packages
    (quote
-    (flow-minor-mode intero haskell-mode indium html-mode company-web company-restclient dired dired-x minions moody which-key org-bullets tide flycheck-irony space-line flycheck-pkg-config cmake-mode evil-magit yasnippet async with-editor mmm-mode vue-html-mode ssass-mode edit-indirect vue-mode bind-key undo-tree tablist rich-minority s faceup quelpa dash f pythonic deferred python-environment epl pkg-info pos-tip popup markdown-mode magit-popup ghub git-commit json-snatcher json-reformat concurrent ctable epc jedi-core helm-core goto-chg evil-org dash-functional auctex shell-mode pdf-tools multi-term yaml-mode latex restclient company-irony irony company-quickhelp quelpa-use-package helm-projectile xclip use-package nlinum evil-commentary json-mode rjsx-mode projectile helm-ag evil-surround smart-mode-line dtrt-indent 0blayout flycheck auto-org-md magit js2-mode company-jedi racket-mode yasnippet-classic-snippets alchemist elixir-mode helm-mode-manager company-go seoul256-theme python-mode react-snippets helm yasnippet-snippets company slime evil elpy)))
+    (highlight-numbers powerline evil-visualstar elpy flycheck-flow evil-matchit smart-parens elixir-yasnippets go-mode web-mode haml-mode flow-minor-mode intero haskell-mode indium html-mode company-web company-restclient dired dired-x minions moody which-key org-bullets tide flycheck-irony space-line flycheck-pkg-config cmake-mode evil-magit yasnippet async with-editor mmm-mode ssass-mode edit-indirect bind-key undo-tree tablist rich-minority s faceup quelpa dash f pythonic deferred python-environment epl pkg-info pos-tip popup markdown-mode magit-popup ghub git-commit json-snatcher json-reformat concurrent ctable epc jedi-core helm-core goto-chg evil-org dash-functional auctex shell-mode pdf-tools eshell yaml-mode latex restclient company-irony irony company-quickhelp quelpa-use-package helm-projectile xclip use-package nlinum evil-commentary json-mode rjsx-mode projectile helm-ag evil-surround dtrt-indent 0blayout flycheck auto-org-md magit js2-mode company-jedi yasnippet-classic-snippets alchemist elixir-mode helm-mode-manager company-go seoul256-theme python-mode react-snippets helm yasnippet-snippets company slime evil)))
  '(scroll-bar-mode nil)
  '(setq ansi-term-color-vector)
  '(tool-bar-mode nil)
@@ -482,7 +505,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Menlo" :foundry "PfEd" :slant normal :weight normal :height 82 :width normal)))))
+ )
 (provide 'init)
 
 ;;; init.el ends here
